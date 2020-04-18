a06a230c4f8b26643e5b77fc8bc03107e371473d
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-18 20:36:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var array1 = [];
+function theBeatlesPlay(){
+
+    for (var i = 0; i < musicians.length; i++) {
+          for (var i = 0; i < instruments.length; i++) {
+                    array1.push(musicians[i]+' '+ 'plays' +' ' +instruments[i])
+}
+}return array1;
+}
+
+
+
+const facts = ["He was the last Beatle to learn to drive",
+      "He was never a vegetarian",
+      "He was a choir boy and boy scout",
+      "He hated the sound of his own voice"];
+
+const facts2 = ["While on tour, he enjoyed playing Monopoly",
+      "Harmonica was the first instrument Lennon learned to play"
+    ];
+
+    var i = 0;
+    var n = 0;
+    var array2 = [];
+    var array3 = [];
+
+
+        function johnLennonFacts()    {
+            while (i < facts.length )   {
+                  array2.push(facts[i++] + '!!!');
+}             while (n < facts2.length )   {
+                  array3.push(facts2[n++] + '!!!');
+}
+        return array2 || array3;
+}
+
+
+
+
+
+
+
+
+
+
+
+var newarr1 = [];
+var n = 0 ;
+function iLoveTheBeatles(n){
+      do{
+        n++;
+  newarr1.push(`I love the Beatles!`)
+}
+while (n < 15);
+if(n > 17){
+return `I love the Beatles!`;
+}
+  return newarr1;
+}
